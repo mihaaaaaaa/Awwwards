01246c7f28880942d9259ddd701b8b9c29195fae
bananaWeb <a89.volkov@gmail.com>
2018-02-22 18:51:06 +0300

    lol

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
-	
+	$('.nav-toggler').click(function(){
+	$('.mobile-nav').toggleClass('-is-open');
+		if ('.mobile-nav') === ('-is-open')(function(){
+
+		});
+	});
 });
